f354141c5930353d41abfdf5eb510560ffc172ff
coderwhen <2250573213@qq.com>
2022-04-19 14:17:57 +0800

    2022-4-19中午提交
    2022-4-19中午提交

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,4 +4,13 @@ Page({
   data: {
     banners: [1,2,3,4,5]
   },
+  onReady() {
+    wx.startPullDownRefresh();
+  },
+  onPullDownRefresh() {
+    console.log('asdasd')
+    setTimeout(() => {
+      wx.stopPullDownRefresh()
+    }, 2000)
+  }
 })
